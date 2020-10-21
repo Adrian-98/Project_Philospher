2bb180c015b22f2508ffbffd1724b72a4a537705
Adrian Munoz perez <amunoz-p@c3r13s6.42madrid.com>
2020-10-21 19:30:46 +0200

    starting the project

---
+++ philo_one/philosopher.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosopher.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amunoz-p <amunoz-p@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/21 18:27:58 by amunoz-p          #+#    #+#             */
+/*   Updated: 2020/10/21 19:24:19 by amunoz-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHER_H
+#define PHILOSOPHER_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <sys/time.h>
+# include <stdio.h>
+
+
+#endif
